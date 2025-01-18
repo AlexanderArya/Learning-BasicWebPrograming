59a4ae86b32667ff63d8cb6bd753ca49b43f60af
Andrean Bimo <bimowibowo4846@gmail.com>
2025-01-18 23:23:56 +0700

    Midterm Test

---
+++ TTS/script.js
@@ -0,0 +1,8 @@
+// Settings Toogle 
+const navbarNav = document.querySelector(".navigasi");
+
+// Ketika Diklik 
+
+const querySelector = document.querySelector(".menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+}
